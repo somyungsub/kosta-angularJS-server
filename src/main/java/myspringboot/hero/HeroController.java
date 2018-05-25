1fde7c66bf6a75ce41fc73db7d237b438a83a990
somyungsub <gkdldy5@naver.com>
2018-05-25 16:50:17 +0900

    Modified : 메서드별 로그 설명 수정 및 추가

--- src/main/java/myspringboot/hero/HeroController.java
+++ src/main/java/myspringboot/hero/HeroController.java
@@ -6,19 +6,20 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/heroes")
 public class HeroController {
 
-	private List<Hero> heros = new ArrayList<>();	
-	
+	private List<Hero> heros = new ArrayList<>();
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	HeroController() {
@@ -34,14 +35,12 @@ public class HeroController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Hero getHero(@PathVariable("id") Long id) {
 		/*
-		 * this.heros = List<Hero>
-		 * this.heros.stream() = Stream<Hero>
-		 * this.heros.stream().filter(boolean) =>  filter 안은 조건
+		 * this.heros = List<Hero> this.heros.stream() = Stream<Hero>
+		 * this.heros.stream().filter(boolean) => filter 안은 조건
 		 * 
 		 * id = @PathVariable("id") Long id, h->h.getId hero 매개변수
 		 * 
-		 * findfirst() => Hero 반환
-		 * orElse(값) -> 없는 경우 디폴트 값 설정
+		 * findfirst() => Hero 반환 orElse(값) -> 없는 경우 디폴트 값 설정
 		 */
 		logger.debug("상세조회 : " + id);
 		return this.heros.stream().filter(h -> h.getId() == id).findFirst().orElse(null);
@@ -50,6 +49,7 @@ public class HeroController {
 	@RequestMapping(method = RequestMethod.POST)
 	public Hero saveHero(@RequestBody Hero hero) {
 		Long nextId = 0L;
+		logger.debug("등록 전 : " + hero);
 		if (this.heros.size() != 0) {
 			/*
 			 * skip -> 매개변수 그사이즈 만큼 건너뛰는 것
@@ -60,6 +60,7 @@ public class HeroController {
 
 		hero.setId(nextId);
 		this.heros.add(hero);
+		logger.debug("등록 후 : " + hero);
 		return hero;
 
 	}
@@ -69,39 +70,44 @@ public class HeroController {
 		/*
 		 * @RequestBody : JSON->Java 객체로 매핑
 		 */
+		logger.debug("업데이트 전 : " + hero);
 		Hero modifiedHero = this.heros.stream().filter(u -> u.getId() == hero.getId()).findFirst().orElse(null);
 		modifiedHero.setName(hero.getName());
+		logger.debug("업데이트 후 : " + modifiedHero);
 		return modifiedHero;
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public boolean deleteHero(@PathVariable Long id) {
+		logger.debug("삭제 : " + id);
 		Hero deleteHero = this.heros.stream().filter(h -> h.getId() == id).findFirst().orElse(null);
 		if (deleteHero != null) {
+			logger.debug("삭제 된 히어로 : " + deleteHero);
 			this.heros.remove(deleteHero);
 			return true;
-		} else  {
+		} else {
 			return false;
 		}
 	}
-	
+
 	// 검색
 	@RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
-	public List<Hero> searchHeroes(@PathVariable String name){
-		return this.heros.stream().filter(h->h.getName().contains(name)).collect(Collectors.toList());
+	public List<Hero> searchHeroes(@PathVariable String name) {
+		logger.debug("검색 : " + name);
+		return this.heros.stream().filter(h -> h.getName().contains(name)).collect(Collectors.toList());
 	}
 
 	void buildHeros() {
-		Hero hero1 = new Hero(11L,"Miss. Nice");
-		Hero hero2 = new Hero(12L,"Narco");
-		Hero hero3 = new Hero(13L,"Bombasto");
-		Hero hero4 = new Hero(14L,"Celeritas");
-		Hero hero5 = new Hero(15L,"Magneta");
-		Hero hero6 = new Hero(16L,"RubberMan");
-		Hero hero7 = new Hero(16L,"Dynama");
-		Hero hero8 = new Hero(18L,"Dr IQ");
-		Hero hero9 = new Hero(19L,"Magma");
-		Hero hero10 = new Hero(20L,"Tornado");
+		Hero hero1 = new Hero(11L, "Miss. Nice");
+		Hero hero2 = new Hero(12L, "Narco");
+		Hero hero3 = new Hero(13L, "Bombasto");
+		Hero hero4 = new Hero(14L, "Celeritas");
+		Hero hero5 = new Hero(15L, "Magneta");
+		Hero hero6 = new Hero(16L, "RubberMan");
+		Hero hero7 = new Hero(16L, "Dynama");
+		Hero hero8 = new Hero(18L, "Dr IQ");
+		Hero hero9 = new Hero(19L, "Magma");
+		Hero hero10 = new Hero(20L, "Tornado");
 
 		heros.add(hero1);
 		heros.add(hero2);
@@ -113,6 +119,6 @@ public class HeroController {
 		heros.add(hero8);
 		heros.add(hero9);
 		heros.add(hero10);
-		
+
 	}
 }
